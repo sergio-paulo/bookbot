26eac0eb291aa047295ab630eb825ab6fd1b134b
sergio.paulo <sergio.e.r.c.paulo@gmail.com>
2025-02-27 15:24:14 +0000

    'Read File' chapter

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+    
+def main():
+    book_path = 'books/frankenstein.txt'
+    book_text = get_book_text(book_path)
+    print(book_text)
+    
+main()
