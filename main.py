f46358ed0f73a29c23c947984713b8feb51de569
sergio.paulo <sergio.e.r.c.paulo@gmail.com>
2025-02-27 15:34:12 +0000

    C: 'Count Words' chapter

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
     
+def get_num_words(text):
+    return len(text.split())
+    
 def main():
     book_path = 'books/frankenstein.txt'
     book_text = get_book_text(book_path)
-    print(book_text)
+    num_words = get_num_words(book_text)
+    print(f"{num_words} words found in the document")
     
 main()
