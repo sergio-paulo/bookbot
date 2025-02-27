aec50d54008d9b6621a2de090e1b5971cb3cfa61
sergio.paulo <sergio.e.r.c.paulo@gmail.com>
2025-02-27 15:42:19 +0000

    E: 'Count Characters' chapter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -10,5 +10,6 @@ def main():
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     print(f"{num_words} words found in the document")
-    
+    char_dict = get_char_count(book_text)
+    print(char_dict)
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     return len(text.split())
 
+def get_char_count(text):
+    char_dict = {}
+    for char in text:
+        char_lower = char.lower()
+        if char_lower not in char_dict:
+            char_dict[char_lower] = 1
+        else:
+            char_dict[char_lower] += 1
+    return char_dict
+
