4399b0457b5b69699ee23dc48d504adb986b44f2
sergio.paulo <sergio.e.r.c.paulo@gmail.com>
2025-02-27 19:18:12 +0000

    G: 'Arguments' chapter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_char_count, get_list_chars_dicts
+import sys
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -6,7 +7,11 @@ def get_book_text(book_path):
     
     
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return  sys.exit(1)
+       
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     char_dict = get_char_count(book_text)
